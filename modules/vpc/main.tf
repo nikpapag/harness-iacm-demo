956d9c8956843b106dc6d27ec4cf5e4894df4f1a
nikpapag <82569427+nikpapag@users.noreply.github.com>
2025-06-27 11:30:37 +0100

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,20 @@
+
+# ------------------------------------------------------------------------------
+# VPC
+# ------------------------------------------------------------------------------
+
+resource "aws_vpc" "harness_iacm_vpc" {
+    cidr_block           = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+
+    tags = {
+        name  = ""${var.name}"
+        owner = "${var.tag_owner}"
+        Name        = "${var.name}"
+        Environment = "${var.environment}"
+        cost_center = "${var.cost_center}"
+        Owner       = "${var.tag_owner}"
+        Project     = "${var.project}"
+        Terraform   = "true"
+    }
+}
